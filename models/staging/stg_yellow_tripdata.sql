2df3f895af53483374f27f6cb3388fa9036c3457
prajwal287@gmail.com <prajwal287@gmail.com>
2026-02-01 21:39:20 +0000

    .

--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -13,6 +13,7 @@ select
         store_and_fwd_flag,
         cast(passenger_count as integer) as passenger_count,
         cast(trip_distance as numeric) as trip_distance,
+        1 as trip_type,
 
         -- payment info
         cast(fare_amount as numeric) as fare_amount,
@@ -21,6 +22,7 @@ select
         cast(tip_amount as numeric) as tip_amount,
         cast(tolls_amount as numeric) as tolls_amount,
         cast(improvement_surcharge as numeric) as improvement_surcharge,
+        0 as ehail_fee,
         cast(total_amount as numeric) as total_amount,
         cast(payment_type as integer) as payment_type
 
